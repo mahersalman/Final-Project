35a994fe0a827b98ad22d557f4174cc783dffb6c
EdenBar01 <edenbar2008@gmail.com>
2024-07-13 16:59:59 +0300

    start the fronend

---
+++ migdalor/src/App.js
@@ -0,0 +1,91 @@
+import "./index.css";
+import { useReducer, useState, useRef, useEffect } from "react";
+import Button from "./components/Button";
+
+
+// //costum hook
+// //hook is a function that we can use for any repeatable code (Reuse)
+// function useInput(initialValue){
+//   const [value, setValue] = useState(initialValue);
+//   return [
+//     {
+//       value, 
+//       onChange: e => setValue(e.target.value)
+//     },
+//     () => setValue(initialValue)
+//   ];
+// }
+
+function GithubUser({name, id, avatar}){
+  return(
+  <div>
+    <h1>{name}</h1>
+    <p>{id}</p>
+    <img src={avatar} height={150} alt={name}/>
+  </div>
+);
+}
+
+function App() {
+  // const [titleProps, resetTitle] = useInput("");
+  // const [colorProps, resetColor] = useState("#000000");
+  // const submit = (e) =>{
+  //   e.preventDefault();
+  //   alert(`${titleProps.value}, ${colorProps.value}`);
+  //   resetTitle();
+  //   resetColor();
+  // };
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://api.github.com/users/edenbar01`)
+    .then((response) => response.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, []);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>
+  if (!data) return null;
+
+  return(
+    // <div>
+    //   <GithubUser 
+    //     name={data.name}
+    //     id={data.id}
+    //     avatar={data.avatar_url}
+    // />
+    <Button
+    type="button"
+    label="click"
+    onClick={null}
+    icon={null}>
+
+    </Button>
+
+    /* </div> */
+
+    
+  )
+
+  return (
+  //  <form onSubmit={submit}> 
+  //   <input 
+  //   {...titleProps}
+  //   type="text" 
+  //   placeholder="color title..."/>
+  //   <input 
+  //   {...colorProps}
+  //   type="color"/>
+  //   <button>ADD</button>
+  //  </form>
+  <h1>Data</h1>
+  );
+}
+
+export default App;
