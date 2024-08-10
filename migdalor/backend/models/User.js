242f9bce7fab974c2ccdc0ed8c7a3e06d4cc3f62
PAZFA <pazfa@rafael.co.il>
2024-08-10 16:38:39 +0300

    back from java to node

--- migdalor/backend/models/User.js
+++ migdalor/backend/models/User.js
@@ -2,9 +2,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  admin: { type: String, enum: ['YES', 'NO'], default: 'NO' },
+  department: { type: String }
 });
 
 const User = mongoose.model('User', UserSchema);
