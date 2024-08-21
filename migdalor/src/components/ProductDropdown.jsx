d79b65ff8697ebbce2582c55a290c1001e7e48fc
pazfayer <pazfayer@gmail.com>
2024-08-21 13:06:40 +0300

    station page work with fillters

--- migdalor/src/components/ProductDropdown.jsx
+++ migdalor/src/components/ProductDropdown.jsx
@@ -8,7 +8,6 @@ useEffect(() => {
     const fetchProducts = async () => {
       try {
         const response = await axios.get('http://localhost:5000/api/products');
-        console.log('Response:', response);
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching products:', error);
